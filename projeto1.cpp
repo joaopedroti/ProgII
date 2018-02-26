f2a75ab83ebcc8e3f9c62724298b947ecc2ea2b9
joaopedroti <pedrin-sg@hotmail.com>
2018-02-26 20:43:11 -0300

    Add files via upload

---
+++ projeto1.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void contagem_regressiva(int ini)
+{
+	
+	int i;
+	for(i=ini;i>0;i--){
+	
+	printf("\n%d", i);
+}
+	printf("\nBoom!!");
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+int main ()
+{
+	
+	
+	contagem_regressiva(10);
+	contagem_regressiva(5);
+	contagem_regressiva(8);
+	
+	return 0;	
+	
+}
